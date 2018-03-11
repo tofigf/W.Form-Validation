40d606ebd0cd300806f960ddd97f6d02892b63b4
tofigf <tofigshf@code.edu.az>
2018-03-11 20:44:12 +0400

    validation

---
+++ Char_validation/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Char_validation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblLow = new System.Windows.Forms.Label();
+            this.lblUpper = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblSpecial = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblEnter = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.BackColor = System.Drawing.Color.White;
+            this.btnClick.Enabled = false;
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClick.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClick.Location = new System.Drawing.Point(354, 333);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(170, 59);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Get Started";
+            this.btnClick.UseVisualStyleBackColor = false;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEmail.Location = new System.Drawing.Point(71, 9);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 20);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPass.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPass.Location = new System.Drawing.Point(71, 204);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(86, 20);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "Password";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.txtEmail.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtEmail.Location = new System.Drawing.Point(75, 32);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(210, 39);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.TextChanged += new System.EventHandler(this.btnClick_Click);
+            this.txtEmail.Enter += new System.EventHandler(this.txtEmail_Enter);
+            this.txtEmail.Leave += new System.EventHandler(this.txtEmail_Leave);
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtpass.Location = new System.Drawing.Point(75, 234);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(210, 20);
+            this.txtpass.TabIndex = 4;
+            this.txtpass.UseSystemPasswordChar = true;
+            this.txtpass.TextChanged += new System.EventHandler(this.btnClick_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.txtName.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txtName.Location = new System.Drawing.Point(75, 135);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(210, 38);
+            this.txtName.TabIndex = 5;
+            this.txtName.TextChanged += new System.EventHandler(this.btnClick_Click);
+            this.txtName.Enter += new System.EventHandler(this.txtName_Enter);
+            this.txtName.Leave += new System.EventHandler(this.txtName_Leave);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblName.Location = new System.Drawing.Point(71, 112);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(55, 20);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Char_validation.Properties.Resources.brave;
+            this.pictureBox1.Location = new System.Drawing.Point(354, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblLow
+            // 
+            this.lblLow.AutoSize = true;
+            this.lblLow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLow.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblLow.Location = new System.Drawing.Point(72, 268);
+            this.lblLow.Name = "lblLow";
+            this.lblLow.Size = new System.Drawing.Size(166, 17);
+            this.lblLow.TabIndex = 8;
+            this.lblLow.Text = "One lowercase character";
+            // 
+            // lblUpper
+            // 
+            this.lblUpper.AutoSize = true;
+            this.lblUpper.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblUpper.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblUpper.Location = new System.Drawing.Point(72, 292);
+            this.lblUpper.Name = "lblUpper";
+            this.lblUpper.Size = new System.Drawing.Size(170, 17);
+            this.lblUpper.TabIndex = 9;
+            this.lblUpper.Text = "One uppercase character";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblNumber.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblNumber.Location = new System.Drawing.Point(74, 316);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(87, 17);
+            this.lblNumber.TabIndex = 10;
+            this.lblNumber.Text = "One number";
+            // 
+            // lblSpecial
+            // 
+            this.lblSpecial.AutoSize = true;
+            this.lblSpecial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSpecial.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSpecial.Location = new System.Drawing.Point(72, 339);
+            this.lblSpecial.Name = "lblSpecial";
+            this.lblSpecial.Size = new System.Drawing.Size(147, 17);
+            this.lblSpecial.TabIndex = 11;
+            this.lblSpecial.Text = "One special character";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMax.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMax.Location = new System.Drawing.Point(73, 366);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(146, 17);
+            this.lblMax.TabIndex = 12;
+            this.lblMax.Text = "8 characters minimum";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(72, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 13;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Maroon;
+            this.lblError.Location = new System.Drawing.Point(72, 95);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 14;
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblEnter.Location = new System.Drawing.Point(54, 183);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(0, 13);
+            this.lblEnter.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(610, 446);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblSpecial);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblUpper);
+            this.Controls.Add(this.lblLow);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.btnClick);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblLow;
+        private System.Windows.Forms.Label lblUpper;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblSpecial;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblEnter;
+    }
+}
+
---
+++ Char_validation/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Char_validation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            string password = txtpass.Text;
+            //password checking
+            if (password.Any(char.IsLower))
+            {
+                lblLow.ForeColor = Color.Gray;
+            }
+            else
+            {
+                lblLow.ForeColor = Color.Black;
+            }
+            if (password.Any(char.IsUpper))
+            {
+                lblUpper.ForeColor = Color.Gray;
+            }
+            else
+            {
+                lblUpper.ForeColor = Color.Black;
+            }
+            if (password.Any(char.IsNumber))
+            {
+                lblNumber.ForeColor = Color.Gray;
+            }
+            else
+            {
+                lblNumber.ForeColor = Color.Black;
+            }
+            if (password.Any(char.IsSymbol))
+            {
+                lblSpecial.ForeColor = Color.Gray;
+            }
+            else
+            {
+                lblSpecial.ForeColor = Color.Black;
+            }
+            if (password.Length > 7)
+            {
+                lblMax.ForeColor = Color.Gray;
+                btnClick.Enabled = true;
+                btnClick.BackColor = Color.LightSeaGreen;
+            }
+            else
+            {
+                lblMax.ForeColor = Color.Black;
+                btnClick.Enabled = false;
+                btnClick.BackColor = Color.White;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_Leave(object sender, EventArgs e)
+        {
+            if (!txtEmail.Text.Contains("@"))
+            {
+                lblError.Text = "An email address must contain a single @";
+            }
+        }
+
+        private void txtEmail_Enter(object sender, EventArgs e)
+        {
+            lblError.Text = " ";
+        }
+
+        private void txtName_Enter(object sender, EventArgs e)
+        {
+           
+                lblEnter.Text = " Choose a username that contains only letters and numbers.";
+            
+        }
+
+        private void txtName_Leave(object sender, EventArgs e)
+        {
+            lblEnter.Text = " ";
+        }
+    }
+}
